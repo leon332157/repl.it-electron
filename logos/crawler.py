9d6d8e13842c865667beb0034ed34f1062fd24a6
frissyn <Ire.Raufu76@gmail.com>
2021-02-05 14:45:50 +0000

    ✨: update helper scripts

--- logos/crawler.py
+++ logos/crawler.py
@@ -1,19 +1,21 @@
-# import json
-# from bs4 import BeautifulSoup as bs
-import requests
 import json
 import base64
+import requests
 
-STRING = open('langs.txt','rb').read()
+
+STRING = open("langs.txt", "rb").read()
 known = json.loads(base64.b64decode(STRING))
+
+head = {
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:64.0) Gecko/20100101 Firefox/64.0 "
+}
 
 for each in known:
     print(each)
-    link = known[each]['icon']
-    if link.startswith('/'):
-        link = 'https://repl.it'+link
-    content = requests.get(f'{link}', headers={
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:64.0) Gecko/20100101 Firefox/64.0 '}).content
-    #print(content)
-    with open(f'logos-raw/{link.split("/")[-1].replace("?", "")}', 'wb') as f:
+    link = known[each]["icon"]
+    link = "https://repl.it" + link if link.startswith("/") else link
+
+    content = requests.get(f"{link}", headers=head).content
+    # print(content)
+    with open(f'logos-raw/{link.split("/")[-1].replace("?", "")}', "wb") as f:
         f.write(content)
