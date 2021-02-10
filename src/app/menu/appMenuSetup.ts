c66e55eebd7af7dea803ecb22ae07aa7f53dc7d3
leon332157 <leon332157@gmail.com>
2021-02-10 14:00:58 -0500

    fix code style

--- src/app/menu/appMenuSetup.ts
+++ src/app/menu/appMenuSetup.ts
@@ -202,7 +202,9 @@ function appMenuSetup(
                 {
                     label: 'Report a bug, or request a feature.',
                     click() {
-                        shell.openExternal('https://github.com/repl-it-discord/repl-it-electron/issues/new/choose');
+                        shell.openExternal(
+                            'https://github.com/repl-it-discord/repl-it-electron/issues/new/choose'
+                        );
                     }
                 }
             ]
